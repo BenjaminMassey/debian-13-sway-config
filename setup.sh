7eb7aaedce5a6f3f1f6a6a51fe952d165533c646
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-10-08 00:56:44 -0700

    initial commit: sway and nvim configs plus general setup script for starting off of fresh deb13 installs

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root (use sudo)"
+  exit 1
+fi
+
+read -p "This is intended as a setup script coming from a base Debian 13 install. Are you sure you know what you're doing? (y/n) " answer
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+  echo "Starting..."
+else
+  echo "Aborted."
+  exit 1
+fi
+
+apt install sway alacritty fuzzel copyq brightnessctl tree-sitter-cli wget curl git
+
+git config --global user.email "benjamin.w.massey@gmail.com"
+git config --global user.name "Benjamin Massey"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup component add rust-analyzer
+cargo install cargo-deb
+
+curl -LO --output-dir ~/Downloads https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+rm -rf /opt/nvim-linux-x86_64
+tar -C /opt -xzf ~/Downloads/nvim-linux-x86_64.tar.gz
+
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Arimo.zip &&
+  cd ~/.local/share/fonts &&
+  unzip Arimo.zip &&
+  rm Arimo.zip &&
+  fc-cache -fv
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+echo "Done! Make sure to run $(nvim) to install and configure."
